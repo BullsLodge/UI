6ed7a8158bfd6a077e2c9920a4bd42b811717f59
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2023-05-12 22:03:34 +0530

    slider section responsive

--- src/pages/Home/Team/Team.jsx
+++ src/pages/Home/Team/Team.jsx
@@ -2,25 +2,67 @@ import Slider from "./components/Slider"
 
 const Team = () => {
   return (
-    <section className="mt-32 md:mt-64 mb-32">
+    <section className="mt-32 md:mt-64 mb-32 m-4">
       <div className="text-center font-Oswald">
-        <h2 className="uppercase font-bold my-2 text-4xl md:text-6xl mb-4">
+        <h2 className="uppercase font-bold my-2  text-5xl md:text-6xl mb-4">
           BULL'S LODGE NFT TEAM
         </h2>
-        <h3 className="text-xl md:text-3xl font-normal mb-12">
+        <h3 className="text-sm md:text-3xl font-normal mb-12 md:mb-24">
           Project welcomes members around the world to share in knowledge about
           the ever-evolving space of NFTs.
         </h3>
       </div>
       <Slider
         team={[
-          { img: "/assets/images/mem1.png", name: "rajnish" },
-          { img: "/assets/images/mem2.png", name: "rajnish2" },
-          { img: "/assets/images/mem3.png", name: "rajnish3" },
-          { img: "/assets/images/mem1.png", name: "rajnish3" },
-          { img:  "/assets/images/mem2.png", name: "rajnish3" },
-          { img:  "/assets/images/mem3.png", name: "rajnish3" },
-          { img:  "/assets/images/mem1.png", name: "rajnish3" },
+          {
+            img: "/assets/images/mem1.png",
+            name: "artist bull",
+            designation: "lead funder",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!",
+          },
+          {
+            img: "/assets/images/mem2.png",
+            name: "artist bull",
+            designation: "lead funder",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!",
+          },
+          {
+            img: "/assets/images/mem3.png",
+            name: "artist bull",
+            designation: "lead funder",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!",
+          },
+          {
+            img: "/assets/images/mem1.png",
+            name: "artist bull",
+            designation: "lead funder",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!",
+          },
+          {
+            img: "/assets/images/mem2.png",
+            name: "artist bull",
+            designation: "lead funder",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!",
+          },
+          {
+            img: "/assets/images/mem3.png",
+            name: "artist bull",
+            designation: "lead funder",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!",
+          },
+          {
+            img: "/assets/images/mem1.png",
+            name: "artist bull",
+            designation: "lead funder",
+            description:
+              "Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, aliquid!",
+          },
         ]}
       />
     </section>
--- src/pages/Home/Team/components/Slider.jsx
+++ src/pages/Home/Team/components/Slider.jsx
@@ -19,11 +19,23 @@ const Slider = ({ team }) => {
         aria-label="Team"
       >
         {team.map((member, index) => (
-          <SplideSlide
-            key={index}
-            className=" rounded-xl  bg-gradient-to-b from-[#FF0099B2] to-[#B845FFB2] "
-          >
-            <img className=" md:h-[300px] h-[180px] " src={member.img} alt="" />
+          <SplideSlide key={index}>
+            <div className=" rounded-xl  bg-gradient-to-b from-[#FF0099B2] to-[#B845FFB2] ">
+              <img
+                className=" md:h-[300px] h-[180px] "
+                src={member.img}
+                alt=""
+              />
+            </div>
+            <h3 className="uppercase font-Oswald font-semibold my-2 text-xl text-center md:text-3xl mb-4">
+              {member.name}
+            </h3>
+            <p className="  font-Oswald leading-3 capitalize my-2 text-lg md:text-2xl mb-4">
+              {member.designation}
+            </p>
+            <p className="font-thin text-xs md:text-base capitalize my-2 mb-4">
+              {member.description}
+            </p>
           </SplideSlide>
         ))}
       </Splide>
