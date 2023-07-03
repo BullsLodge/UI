930942f693dfd65227c512003f810cbe1c52cd09
Rajnish Pratap Singh <rps.rajnish@gmail.com>
2023-07-04 00:05:23 +0530

    content

--- src/pages/Home/Collection/Collection.jsx
+++ src/pages/Home/Collection/Collection.jsx
@@ -4,10 +4,10 @@ const Collection = () => {
     <section id="collection" className="mt-32 md:mt-64 mb-32 md:mb-64 m-4">
       <div className="text-center font-Oswald">
         <h2 className="uppercase font-bold my-2  text-5xl md:text-6xl mb-4">
-          OUR LODGE'S NFT COLLECTION
+        AN UNMATCHED NFT COLLECTION FOR YOU!
         </h2>
         <h3 className="text-sm md:text-2xl mt-4 font-normal">
-          Each Bull features a combination of unique traits,
+          Each Bull features a combination of unique attributes,
         </h3>
         <h3 className="text-sm md:text-2xl font-normal mb-12 md:mb-24">
           handcrafted in signature style by our in-house digital artists.
--- src/pages/Home/Hero/Hero.jsx
+++ src/pages/Home/Hero/Hero.jsx
@@ -20,24 +20,24 @@ const Hero = () => {
 
         <div>
           <h1 className="zoom-in-text px-4 md:px-0 text-5xl md:text-7xl leading-relaxed md:leading-snug transition-all font-bold mb-4">
-            THE BULLS ARE
-            <br /> RAGING...
+            GET BULLISH
+            <br /> NOW!
           </h1>
           <h2 className="hidden md:block md:text-xl 2xl:text-4xl mb-2">
-            Bull's Lodge NFT;
+            Bull's Lodge NFT
           </h2>
           <h2 className="hidden md:block md:text-xl 2xl:text-3xl mb-2">
-            Home of your benefits in
+            Discover Your Privileges
           </h2>
           <h2 className="hidden md:block md:text-xl 2xl:text-3xl mb-16">
-            Metaverse & Reality. MINT to be announced soon.
+            By Becoming Bull!
           </h2>
         </div>
       </div>
       <div className=" md:flex hidden justify-center">
         <Link to="/mint">
           <button className="font-Oswald font-bold border-4 hover:bg-gradient-to-b hover:from-[#EA00EF]  hover:to-[#FF0099]  border-[#FF0099] p-8 mb-4 text-5xl">
-            MINT TBA
+            MINT NOW
           </button>
         </Link>
       </div>
@@ -45,9 +45,9 @@ const Hero = () => {
         01:23:45
       </p>
       <div className="md:hidden font-Oswald text-center text-xl mt-6">
-        <h2 className="text-2xl">Bull's Lodge NFT;</h2>
-        <h2 className="">Home of your benefits in</h2>
-        <h2 className="">Metaverse & Reality. MINT to be announced soon.</h2>
+        <h2 className="text-2xl">Bull's Lodge NFT</h2>
+        <h2 className="">Discover Your Privileges</h2>
+        <h2 className="">By Becoming Bull!</h2>
       </div>
       <div
         data-aos="slide-right"
